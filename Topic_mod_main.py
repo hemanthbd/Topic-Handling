cb71dcb24a79e1ff4b0c2d5063b3559c5fc9f464
Hemanth Balaji <34942185+hemanthbd@users.noreply.github.com>
2018-10-02 07:12:32 -0700

    Update2

--- Topic_mod_main.py
+++ Topic_mod_main.py
@@ -1,4 +1,4 @@
-# Program to perform Topic-Handling for n Documents using LDA (Dictionary is the Entire Corpus)
+# Program to perform Topic-Modeling for n Documents using LDA (Dictionary is the Entire Corpus)
 
 import json
 from pprint import pprint
@@ -44,6 +44,7 @@ final = []
 data = []
 results = []
 
+print("\nOpening Input json file")
 # Open the Input .json file which contains the documents
 with open('topic_modeling_data.json') as f:
 	dic = [json.loads(line) for line in f]
@@ -57,24 +58,27 @@ for i in range(0,len(dic)):
 # Loop to convert into Lowercase
 for i in range(0,len(doc_list)):
 	raw.append(doc_list[i].lower())
-print('Original',len(raw))
+print('\nTotal Number of Documents',len(raw))
 
 # Loop to remove Numbers and few symbols
 for i in range(0,len(raw)):
 	only_raw.append(re.sub('[0-9]+', '',raw[i]))
+print('Removed Numbers/Symbols')
 
 # Loop to convert each word into a token
 for i in range(0,len(only_raw)):
 	tokens.append(tokenizer.tokenize(only_raw[i]))
+print('Tokenized the Data')
 
-# Loop to sotre all the tokens eacept those present in stopwords
+# Loop to collect all the tokens except stopwords
 for i in range(0,len(tokens)):
 	new.append([p for p in tokens[i] if not p in stopwords])
+print('Tokenized Data - Stop-Words')
 
 #Loop to 'Lemmatize' each word, ie, Stemm the words, but according to context.
 for i in range(0,len(new)):
 	final.append([lem.lemmatize(p) for p in new[i]])
-
+print('Data Stemmed')
 
 imp1=[]
 results = []
@@ -85,14 +89,15 @@ total_score=[]
 
 # Calculate Dictionary for the Entire Dataset of Documents
 dictionary = corpora.Dictionary(final)
-
+print('\nDictionary Set')
 # Perform K-Means clustering or Bag of Words clustering for Entire set of Documents, and create a Corpus.
 corpus = [dictionary.doc2bow(text) for text in final]
-
+print('Corpus Set')
 
 # Create an LDA MultiCoreModel and pass the Corpus and Dictionary to it, and choose 5 topics with 40 passes and workers=3 (Parallelize)
 ldamodel = gensim.models.LdaMulticore(corpus, num_topics=30, id2word = dictionary, passes=40, minimum_probability=0, workers=3)
-print('Done!!!!!!!')
+print('\n LDA Model Defined')
+print('\n')
 
 # Loop through every Document
 for i in range(0,len(final)):
@@ -111,16 +116,16 @@ for i in range(0,len(final)):
 	topics =[]
 	tsc=[]
 
-	print("Done",i)
+	print("Topics collected for the {}st Document".format(i))
 
 
 total_score= np.asarray(total_score)
-#print('\n Scores', total_score)
+#print('\n Scores for each Topic per Document', total_score)
 
 words=[]
 words1 = []
 
-# Iterate through every topic , append first 5 topics based on Sscore, and remove Numbers (which are the Probab. Dist of each word in the corpus) and any other symbol like spaces or '*'
+# Iterate through every topic , append first 5 topics based on Score, and remove Numbers (which are the Probab. Dist of each word in the corpus) and any other symbol like spaces or '*'
 for i in range(0,len(final)):
 	for j in range(0,5):
 		words1.append(re.sub (r'([^a-zA-Z ]+?)', '', total[i][j]))
@@ -130,15 +135,17 @@ for i in range(0,len(final)):
 
 d2 = []
 
+print("\n Writing into Json file")
+
 # Store every topic in a Dictionary format to export into a .json file
 for i in range(0,len(final)):  
 	d = {'_id': dic[i]['_id'],'topics': [words[i][0], words[i][1], words[i][2],words[i][3],words[i][4]]} 
 	d2.append(d)
 
 # Write into an empty .json file with format: {"_id:":"topics":["topic1","topic2","topic3","topic4","topic5"]} with in Order of Decreasing Scores
-with open('output_tm_main.json', 'w') as outfile:  
+with open('tr3.json', 'w') as outfile:  
     for item in d2:
             x = json.dumps(item,sort_keys=True)
             outfile.write(x+'\n')
 
-print('done')
+print('\n Task End')
